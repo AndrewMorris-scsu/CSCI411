29d6e081bd45c597c5037fa8f3d916779591a8d4
Andrew <moan1303@stcloudstate.edu>
2016-10-26 20:38:14 -0500

    Modified Schema, updated data generator file

--- createTable.sql
+++ createTable.sql
@@ -17,8 +17,8 @@ CREATE TABLE Publications (
 pid integer,
 Title CHAR(30),
 type CHAR(30),
-content CHAR(100),
-cost real,
+content CHAR(50),
+price real,
 PRIMARY KEY(pid) );
 
 CREATE TABLE Writes (
@@ -64,6 +64,24 @@ PRIMARY KEY(perid,pid),
 FOREIGN KEY(perid) REFERENCES Customer,
 FOREIGN KEY(pid) REFERENCES Publications );
 
+--INSERT VALUES__
+
+INSERT INTO Persons (perid) VALUES (1);
+INSERT INTO Persons (perid) VALUES (2);
+INSERT INTO Persons (perid) VALUES (3);
+INSERT INTO Persons (perid) VALUES (4);
+INSERT INTO Persons (perid) VALUES (5);
+INSERT INTO Publications (pid, Title, type, Content, price) VALUES (1, 'Posuere libero.', 'Transactions', 'Neque, torquent duis.', 724002.91);
+INSERT INTO Publications (pid, Title, type, Content, price) VALUES (2, 'Penatibus tempor!', 'Conference Proceedings', 'Quis ipsum habitant.', 594202.30);
+INSERT INTO Publications (pid, Title, type, Content, price) VALUES (3, 'Duis dictum.', 'Transactions', 'Mauris praesent lacus!', 614066.00);
+INSERT INTO Customer (perid) VALUES (3);
+INSERT INTO Customer (perid) VALUES (4);
+INSERT INTO Customer (perid) VALUES (5);
+INSERT INTO Authors (perid, city, state, streetAddress) VALUES (1, 'irving', 'tx', '6772 hortense');
+INSERT INTO Authors (perid, city, state, streetAddress) VALUES (2, 'madison', 'wi', '1645 cregier');
+
+
+
 --DELETE TABLES__
 
 DROP TABLE Rates;
@@ -75,3 +93,4 @@ DROP TABLE Writes;
 DROP TABLE Publications;
 DROP TABLE Authors;
 DROP TABLE Persons;
+
