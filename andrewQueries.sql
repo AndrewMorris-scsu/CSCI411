1ce9fb8d89c9bce77053ee22589c7024f3ba8b48
Andrew <moan1303@stcloudstate.edu>
2016-10-30 21:40:45 -0500

    Updated Query file

--- andrewQueries.sql
+++ andrewQueries.sql
@@ -1,7 +1,33 @@
+CREATE OR REPLACE PROCEDURE getBook(personID in INTEGER, get_pid in INTEGER)
+AS 
+book integer;
+person integer;
+isAuthor CHAR;
+hasPayed CHAR;
+isBook CHAR;
 
--- Function that returns 'T' if the person exists, otherwise 'F'
-create or replace FUNCTION bookExists(book_pid in INTEGER)
-RETURN CHAR
+BEGIN
+  --Check that the book exists
+    isAuthor := bookExists( 1, 2);
+  if bookExists(get_pid) = 'F' then
+    DBMS_OUTPUT.PUT_LINE('The Book does not exist...');
+    RETURN;
+  else
+    if personExists(personID) = 'F' then
+        DBMS_OUTPUT.PUT_LINE('The Person does not exist...');
+        RETURN; 
+    else
+        DBMS_OUTPUT.PUT_LINE('Continue');
+    END IF; 
+     
+        
+  END IF;  
+
+END;
+--Function that returns a T if the book exists, otherwise
+--it will return an 'F'
+CREATE OR REPLACE FUNCTION bookExists(book_pid in INTEGER)
+RETURN CHAR(1)
   IS
     book integer; 
   BEGIN
@@ -10,13 +36,14 @@ RETURN CHAR
     WHERE P.pid = book_pid;
     
     IF book IS NULL THEN
-       return 'F';
+       return 'F'
     ELSE
-       return 'T';
+       return 'T'
     END IF;
 END bookExists;
 
--- Function that returns 'T' if the person exists, otherwise 'F'
+--Function that returns a T of the person exists, otherwise
+--it will return an ''
 create or replace FUNCTION personExists(person_perid in INTEGER)
 RETURN CHAR
   IS
@@ -33,25 +60,5 @@ RETURN CHAR
     END IF;
 END personExists;
 
--- Procedure to getBook for a person
-CREATE OR REPLACE PROCEDURE getBook(personID in INTEGER, get_pid in INTEGER)
-AS 
-book integer;
-person integer;
-isAuthor CHAR(1);
-hasPayed CHAR(1);
 
-BEGIN
-  --Check that the book exists
-  Select p.pid into book
-  FROM Publications P
-  WHERE P.pid = get_pid;
-  
-  IF book is null THEN
-    DBMS_OUTPUT.PUT_LINE('The book does not exist!!');
-    EXIT;
-  ELSIF 
-  end if;
-
-END;
 
