7dfb113e7b8e0bd94bb1dd50bddfe05744d90aa6
Yulduz <big_bug_95@mail.ru>
2016-10-30 21:43:37 -0500

    Added test data

--- createTable.sql
+++ createTable.sql
@@ -71,12 +71,20 @@ INSERT INTO Persons (perid, name) VALUES (2, 'Yulduz');
 INSERT INTO Persons (perid, name) VALUES (3, 'Aman');
 INSERT INTO Persons (perid, name) VALUES (4, 'Erik');
 INSERT INTO Persons (perid, name) VALUES (5, 'Dude');
+INSERT INTO Persons (perid, name) VALUES (6, 'Mala');
+INSERT INTO Persons (perid, name) VALUES (7, 'Jake');
+INSERT INTO Persons (perid, name) VALUES (8, 'Kate');
+INSERT INTO Persons (perid, name) VALUES (9, 'John');
+INSERT INTO Persons (perid, name) VALUES (10, 'Drake');
 
 --Publications
 INSERT INTO Publications (pid, Title, type, Content, price) VALUES (1, 'Posuere libero.', 'Transactions', 'Neque, torquent duis.', 10);
 INSERT INTO Publications (pid, Title, type, Content, price) VALUES (2, 'Penatibus tempor!', 'Conference Proceedings', 'Quis ipsum habitant.', 20);
 INSERT INTO Publications (pid, Title, type, Content, price) VALUES (3, 'Duis dictum.', 'Transactions', 'Mauris praesent lacus!', 30);
-
+INSERT INTO PUBLICATIONS (pid, title, type, content, price) VALUES (4,'The end of poverty','Journal','Solving pressing problems', 35);
+INSERT INTO PUBLICATIONS (pid, title, type, content, price) VALUES (5, 'Automobile', 'Magazine', 'Auto staff', 5);
+INSERT INTO PUBLICATIONS (pid, title, type, content, price) VALUES (6, 'Dash Diet', 'Article', 'Health issues', 9);
+INSERT INTO PUBLICATIONS (pid, title, type, content, price) VALUES (7, 'Nature', 'Journal', 'Science and Technology', 20);
 --Customers
 INSERT INTO Customer (perid) VALUES (3);
 INSERT INTO Customer (perid) VALUES (4);
@@ -85,6 +93,14 @@ INSERT INTO Customer (perid) VALUES (5);
 --authors
 INSERT INTO Authors (perid, city, state, streetAddress) VALUES (1, 'irving', 'tx', '6772 hortense');
 INSERT INTO Authors (perid, city, state, streetAddress) VALUES (2, 'madison', 'wi', '1645 cregier');
+INSERT INTO AUTHORS (perid,city,state,streetAddress) VALUES (3,'St. Cloud', 'MN', '342 Street A');
+INSERT INTO AUTHORS (perid,city,state,streetAddress) VALUES (4, 'Duluth', 'MN','566 1st Avenue');
+INSERT INTO AUTHORS (perid, city,state,streetAddress) VALUES (5,'Minneapolis', 'MN', '892 Street M');
+INSERT INTO AUTHORS (perid, city, state, streetAddress) VALUES (6, 'St. Paul', 'MN', '555 5th Avenue');
+INSERT INTO AUTHORS (perid, city, state, streetAddress) VALUES (7, 'irving', 'tx', '6780 hortense');
+INSERT INTO AUTHORS (perid, city, state, streetAddress) VALUES (8, 'Duluth', 'MN', '8978 Street B');
+INSERT INTO AUTHORS (perid, city, state, streetAddress) VALUES (9, 'madison', 'wi', '1988 Asher');
+INSERT INTO AUTHORS (perid, city, state, streetAddress) VALUES (10, 'Duluth', 'MN', '888 East Jackson');
 
 --paysFor
 INSERT INTO PaysFor (perid, pid, cost) VALUES (3, 1, 10);
