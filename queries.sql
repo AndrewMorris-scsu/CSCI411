b25ae318e790e196406a628b1ef098d926f0f8e7
Andrew <moan1303@stcloudstate.edu>
2016-11-02 11:34:13 -0500

    Pushing modded quieries

--- queries.sql
+++ queries.sql
@@ -129,6 +129,7 @@ create or replace PROCEDURE GetLatestRetrievals(numberOfEntries IN INTEGER)
 END;
 
 -- 5. LIST REFERENCE TRACE FOR A PUBLICATION ---
+--TODO: Clean up output.
 create or replace procedure findCircle (
       p_start_with IN integer,
       childsParent IN integer,
@@ -169,6 +170,7 @@ create or replace procedure findCircle (
 
   --#6 List the perid of the Author who has max number of publications
 --NOT WORKING
+--TODO: Add a temp table and load to get max number of publications.
   Select A.perid, P.name, count(*) AS "Number of Publications"
     from Authors A, Persons P, Writes Wr
   Where
@@ -198,23 +200,145 @@ FROM Publications P
 Where P.type ='Journal' OR P.TYPE='Magazine'
 ORDER BY P.type;
 
---10.
+--10. Inserts an entry in "PaysFor" for the given pid and perid if it passes a series of checks.
+--Also, if the user is not in customers, as soon as they buy a Pulication for the first time, they
+--become a customer.
+
+create or replace PROCEDURE pay (buyerPerid in INTEGER, buy_pid in INTEGER)
+AS
+bookwriter integer;
+checkp integer;
+amount real;
+isCustomer integer;
+BEGIN
+  --Check that the book and the person exists
+  if (bookExists(buy_pid)) = 'T' and (personExists(BuyerPerid)) = 'T' THEN
 
 
+    -- Check to make sure the person is not the author of this
+    --particular article.
+    Select MAX(A.perid) INTO bookwriter
+    FROM Writes W, Publications P, Persons Per, Authors A
+    WHERE Per.perid = A.perid AND A.perid = W.perid AND W.pid = P.pid
+          AND P.pid = buy_pid AND buyerPerid = P.perid;
 
+    if bookwriter IS NOT NULL THEN
+      DBMS_OUTPUT.PUT_LINE('The author of the book does not need to pay!');
+    else
 
+      --Check that the person has not already bought the publication
+      Select MAX(C.perid) INTO checkp
+      FROM Persons P, Customer C, PaysFor PF
+      WHERE buyerPerid = P.perid AND P.perid = C.perid and C.perid = PF.perid AND PF.pid = buy_pid;
 
+      if checkp IS NOT NULL THEN
+        DBMS_OUTPUT.PUT_LINE('This user has previously bought this publication!');
+      else
+        -- insert into customers if they have not yet bought a publication
+        Select MAX(Cust.perid) INTO isCustomer
+        FROM Customer Cust
+        WHERE Cust.perid = buyerPerid;
 
+        if isCustomer IS NULL THEN
+          DBMS_OUTPUT.PUT_LINE('First time buyer, insert record into Customer. ');
+          INSERT INTO Customer (perid) VALUES (buyerPerid);
+        END IF;
 
+        Select MAX(P.price) INTO amount
+        FROM Publications P
+        WHERE P.pid = buy_pid;
 
--- 2. List the 10 latest retrieved books
+        INSERT INTO paysFor (perid, pid, cost) VALUES (buyerPerid, buy_pid, amount);
+        DBMS_OUTPUT.PUT_LINE('Value was inserted successfully.');
+      END IF;
+    END IF;
+  END IF;
+END;
+
+-- 11. What is the Average cost that each Author charges for there books. Please print
+--out the Author's Name as well as the Average cost of thier books for each of them.
+
+SELECT DISTINCT S.name, AVG(P.price)
+FROM Persons S, Authors A, Writes W, Publications P
+WHERE S.perid = A.perid AND  A.perid = W.perid AND W.pid = P.pid
+GROUP BY S.name;
+
+-- 12. Whats the name of each book that the user X payed to view. Where X is a input parameter.
+
+create or replace PROCEDURE bookName(X IN CHAR) AS
+n CHAR(30);
+cursor books is SELECT DISTINCT P.Title
+FROM Persons S, Customer C, paysFor F, Publications P
+WHERE S.name = X AND S.perid = C.perid AND C.perid = F.perid AND F.pid = P.pid;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('the Publications the user saw was/were named:');
+  open books;
+  loop
+  fetch books INTO n;
+    if(books%NOTFOUND) then
+      exit;
+    end if;
+  DBMS_OUTPUT.PUT_LINE(n);
+end loop;
+close books;
+END;
+
+-- 13. For each Author find the Average rating give for each of their books. Print the Authors Name and the Average rating
+--TODO: Can have 2 DISTINCT authors with the same name.
+SELECT DISTINCT S.name, AVG(R.rating)
+FROM Persons S, Authors A, Writes W, Publications P, Rates R
+WHERE S.perid = A.perid AND A.perid = W.perid AND W.pid = P.pid AND P.pid = R.pid
+GROUP BY S.name;
+
+-- 14. For Book X find each date for which the book was viewed. where X is a input parameter
+CREATE OR REPLACE PROCEDURE datesviewed(X IN CHAR) AS
+d CHAR(30);
+cursor viewed is SELECT DISTINCT L.dateViewed
+FROM Publications P, RetrieveLog L
+WHERE P.Title = X AND P.pid = L.pid;
+BEGIN
+DBMS_OUTPUT.PUT_LINE('The dates the Book was viewed are :');
+open viewed;
+  loop fetch viewed into d;
+    if(viewed%NOTFOUND) then
+      exit;
+    end if;
+    DBMS_OUTPUT.PUT_LINE(d);
+  end loop;
+close viewed;
+END;
+
+-- 15. Perform checks, and then insert into the write table.
+CREATE OR REPLACE PROCEDURE Write (PERSONID IN INTEGER, write_pid in INTEGER)
+AS
+isAlreadyAuthor integer;
+BEGIN
+  IF personExists(personid) = 'F' then
+    DBMS_OUTPUT.PUT_LINE('The author does not exist.');
+  ELSIF bookExists(write_pid) = 'F' then
+    DBMS_OUTPUT.PUT_LINE('The Publication does not exist.');
+  ELSE
+    Select MAX(W.pid) INTO isAlreadyAuthor
+    FROM Writes W
+    WHERE W.pid = write_pid AND W.perid = PERSONID;
+    if isAlreadyAuthor IS NULL THEN
+      INSERT INTO writes (PID, PERID) VALUES (PERSONID, WRITE_PID);
+      DBMS_OUTPUT.PUT_LINE('Value inserted successfully');
+    ELSE
+      DBMS_OUTPUT.PUT_LINE('This author already exist for this book.');
+    END IF;
+  END IF;
+  COMMIT;
+END;
+
+-- 16.
+
 -- 3. List the counts of views by document type from all Persons. If they have a count of zero for views, show a zero.
 -- 4. What is the average cost for an author's work.
 -- 5. Name of books that that x has read along with the amount of times they have retrieved the work.
 -- 6. Show each person who has bought a book or published an article. Show the count of how many books a person has written, and also show the number of books that a customer has bought. If there is no amount, show a zero.
 --7. Show all users that have read the book with the highest average rating.
 
---8. Print out the names of all of the Customers that have viewed book X.
 --#4 List the most popular book by ascending order
 --#5 List the name & id of Author and Customer who are from the same state
 --#7 List the name, id & date viewed of publications that are rated more than 3 and
